d61e7df853f24d5e91b5100591bf9d1e4b7f6c68
Alisa <alisa.melissa@icloud.com>
2024-01-26 20:51:44 +0200

    Initial Commit

---
+++ ArtParallaxCarousel/ArtParallaxCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtParallaxCarouselApp.swift
+//  ArtParallaxCarousel
+//
+//  Created by Алиса Третьякова on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtParallaxCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtParallaxCarousel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArtParallaxCarousel
+//
+//  Created by Алиса Третьякова on 26.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
